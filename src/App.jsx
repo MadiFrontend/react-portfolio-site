4bcfe2492cc713158a07b8cffc9f62ee7b0a59fe
mahdi <mahdihosseinidev@gmail.com>
2022-04-08 13:40:58 +0430

    Start

---
+++ src/App.jsx
@@ -0,0 +1,13 @@
+import React from "react";
+// Add Components
+import Header from "./components/header/Header";
+
+const App = () => {
+    return (
+        <>
+            <Header />
+        </>
+    );
+};
+
+export default App;
---
+++ src/components/testmonials/Testmonials.jsx
@@ -0,0 +1,10 @@
+import React from 'react'
+import './testmonials.css'
+
+const Testmonials = () => {
+  return (
+    <div>Testmonials</div>
+  )
+}
+
+export default Testmonials
