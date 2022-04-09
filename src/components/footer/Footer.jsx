08abc507e56cceaf986d244c001797a7e723afce
mahdi <mahdihosseinidev@gmail.com>
2022-04-09 19:50:28 +0430

    footer part

--- src/components/footer/Footer.jsx
+++ src/components/footer/Footer.jsx
@@ -1,9 +1,36 @@
 import React from "react";
 import "./footer.css";
+import {FaFacebookF} from 'react-icons/fa';
+import {FiInstagram} from 'react-icons/fi';
+import {IoLogoTwitter} from 'react-icons/io';
+
+
 
 const Footer = () => {
     return (
-    <section id="footer">Footer</section>
+    <footer id="footer">
+        <a href="#" className="footer__logo">Madi</a>
+
+        <ul className="permalinks">
+            <li><a href="#">Home</a></li>
+            <li><a href="#about">About</a></li>
+            <li><a href="#experience">Experience</a></li>
+            <li><a href="#services">Services</a></li>
+            <li><a href="#portfolio">Portfolio</a></li>
+            <li><a href="#testimonial">Testimonial</a></li>
+            <li><a href="#contact">Contact</a></li>
+        </ul>
+
+        <div className="footer__socials">
+            <a href="https://facbook.com"><FaFacebookF /></a>
+            <a href="https://instagram.com"><FiInstagram /></a>
+            <a href="https://twitter.com"><IoLogoTwitter /></a>
+        </div>
+
+        <div className="footer__copyright">
+            <small>&copy; MADI PORTFOLIO. All Raights Reserved.</small>
+        </div>
+    </footer>
     )
 };
 
