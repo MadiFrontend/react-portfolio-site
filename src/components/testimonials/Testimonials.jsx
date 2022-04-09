6f80aa32420e7dbacee1b7f67f7c6e75c33297b5
mahdi <mahdihosseinidev@gmail.com>
2022-04-09 17:18:23 +0430

    testimonials part

--- src/components/testimonials/Testimonials.jsx
+++ src/components/testimonials/Testimonials.jsx
@@ -1,10 +1,69 @@
-import React from 'react'
-import './testimonials.css'
+import React from "react";
+import "./testimonials.css";
+import AVTR1 from "../../assets/avatar1.jpg";
+import AVTR2 from "../../assets/avatar2.jpg";
+import AVTR3 from "../../assets/avatar3.jpg";
+import AVTR4 from "../../assets/avatar4.jpg";
+
+// import Swiper core and required modules
+import { Pagination } from "swiper";
+
+import { Swiper, SwiperSlide } from "swiper/react";
+
+// Import Swiper styles
+import "swiper/css";
+import "swiper/css/pagination";
+
+const data = [
+    {
+        avatar: AVTR1,
+        name: "Tina snow",
+        review: "Madi was a real pleasure to work with and we look forward to working with him again. He’s definitely the kind of developer you can trust with a project from start to finish.",
+    },
+    {
+        avatar: AVTR2,
+        name: "Tina fall",
+        review: "Madi was a real pleasure to work with and we look forward to working with him again. He’s definitely the kind of developer you can trust with a project from start to finish.",
+    },
+    {
+        avatar: AVTR3,
+        name: "Pascal Tremblay",
+        review: "Madi was a real pleasure to work with and we look forward to working with him again. He’s definitely the kind of developer you can trust with a project from start to finish.",
+    },
+    {
+        avatar: AVTR4,
+        name: "Tina spring",
+        review: "Madi was a real pleasure to work with and we look forward to working with him again. He’s definitely the kind of developer you can trust with a project from start to finish.",
+    },
+];
 
 const Testmonials = () => {
-  return (
-    <section id='testimonials'>Testmonials</section>
-  )
-}
+    return (
+        <section id="testimonials">
+            <h5>Review from clients</h5>
+            <h2>Testomonials</h2>
+            <Swiper
+                className="container testimonials__container"
+                // install Swiper modules
+                modules={[Pagination]}
+                spaceBetween={40}
+                slidesPerView={1}
+                pagination={{ clickable: true }}
+            >
+                {data.map(({ avatar, name, review }, index) => {
+                    return (
+                        <SwiperSlide key={index} className="testimonial">
+                            <div className="client__avatar">
+                                <img src={avatar} />
+                            </div>
+                            <h5 className="client__name">{name}</h5>
+                            <small className="client__review">{review}</small>
+                        </SwiperSlide>
+                    );
+                })}
+            </Swiper>
+        </section>
+    );
+};
 
-export default Testmonials
+export default Testmonials;
